7e60dc2c789a0eddb58fcfe37c320ac0ac917b3b
Mateusz Kocz <mateusz@kocz.pl>
2015-02-01 22:03:01 +0100

    Refactored and atomized the Action Sheet module

--- www/views/action-sheet/action-sheet.js
+++ www/views/action-sheet/action-sheet.js
@@ -9,65 +9,86 @@ angular.module( 'app.views.action-sheet', ['ionic', 'ngCordova'] )
 				}
 			}
 		} );
-	} ).controller( 'ActionSheetViewController', ['$ionicActionSheet', '$cordovaActionSheet', function ( $ionicActionSheet, $cordovaActionSheet ) {
-		var self = this;
+	} )
+	.value( 'ionicASProperties', {
+		cancelOnStateChange: true, // This is the default. Just left for reference.
+		cssClass: 'custom-class', // Adds a custom class to the sheet.
+		buttons: [
+			{text: '<b>Bold Button</b>'},
+			{text: 'Normal button'},
+			{text: '<i class="icon ion-game-controller-b"></i> Icon button'}
+		],
+		destructiveText: 'Warning!',
+		titleText: 'Ionic Action Sheet',
+		cancelText: 'Close the sheet'
+	} )
+	.value( 'cordovaASProperties', {
+		title: 'Cordova Action Sheet',
+		buttonLabels: [
+			'<b>Bold Button</b>',
+			'Normal button',
+			'<i class="icon ion-game-controller-b"></i> Icon button'
+		],
+		addCancelButtonWithLabel: 'Close the sheet',
+		androidEnableCancelButton: true,
+		addDestructiveButtonWithLabel: 'Warning!'
+	} )
+	.controller( 'ActionSheetViewController',
+	['$ionicActionSheet', '$cordovaActionSheet', 'ionicASProperties', 'cordovaASProperties',
+		function ( $ionicActionSheet, $cordovaActionSheet, ionicASProperties, cordovaASProperties ) {
 
-		// The Ionic's Action Sheet
-		this.actionTakenInIonicSheet = '';
+			var self = this;
 
-		this.showIonicActionSheet = function () {
-			var hideSheet = $ionicActionSheet.show({
-				cancelOnStateChange: true, // This is the default. Just left for reference.
-				cssClass: 'custom-class', // Adds a custom class to the sheet.
-				buttons: [
-					{ text: '<b>Bold Button</b>' },
-					{ text: 'Normal button' },
-					{ text: '<i class="icon ion-game-controller-b"></i> Icon button'}
-				],
-				destructiveText: 'Warning!',
-				titleText: 'Ionic Action Sheet',
-				cancelText: 'Close the sheet',
-				cancel: function () {
-					// Function will be called when the sheet will have been closed.
-				},
-				buttonClicked: function ( index, button ) {
-					self.actionTakenInIonicSheet = 'Clicked the ' + button.text;
+			// The Ionic's Action Sheet
+			this.actionTaken = '';
 
-					// Don't hide the sheet.
-					return false;
-				},
-				destructiveButtonClicked: function () {
-					self.actionTakenInIonicSheet = 'Clicked the destructive button';
+			// Action taken after when the action sheet is hidden.
+			this.cancelAction = function () {
+				self.actionTaken = 'Clicked cancel button';
+			};
 
-					// Hide the sheet.
-					return true;
-				}
-			});
-		};
+			// Normal button action.
+			this.buttonAction = function ( index, button ) {
+				self.actionTaken = 'Clicked the ' + button.text;
 
-		// Cordova's Action Sheet
-		this.actionTakenInCordovaActionSheet = '';
+				// Don't hide the sheet (only applies to the Ionic's implementation).
+				return false;
+			};
 
-		this.showCordovaActionSheet = function () {
-			// The HTML doesn't work. Just used for consistency.
-			var buttons = [ '<b>Bold Button</b>', 'Normal button', '<i class="icon ion-game-controller-b"></i> Icon button'];
+			// Destructive button action.
+			this.destructiveAction = function () {
+				self.actionTaken = 'Clicked the destructive button';
 
-			$cordovaActionSheet.show({
-				title: 'Cordova Action Sheet',
-				buttonLabels: buttons,
-				addCancelButtonWithLabel: 'Close the sheet',
-				androidEnableCancelButton: true,
-				addDestructiveButtonWithLabel: 'Warning!'
-			} ).then( function ( index ) {
-				// First button will be the destructive one.
+				// Hide the sheet (only applies to the Ionic's implementation).
+				return true;
+			};
+
+			// Normalizes the AS cordova plugin way of handling the buttons index.
+			this.cordovaButtonActionProxy = function ( index ) {
+				if ( !index ) return;
+
+				// The first button is the destructive one.
 				if ( index === 1 )
-					self.actionTakenInCordovaActionSheet = 'Clicked the destructive button';
+					return self.destructiveAction();
+
 				// The last button is the close button.
-				else if ( index === buttons.length + 2 )
-					self.actionTakenInCordovaActionSheet = 'Clicked cancel button';
-				// Indexes in between will be the normal buttons.
-				else
-					self.actionTakenInCordovaActionSheet = buttons[ index - 2 ];
-			});
-		}
-	}] );
+				if ( index === cordovaASProperties.buttonLabels.length + 2 )
+					return self.cancelAction();
+
+				// Indexes in between are the normal buttons.
+				var idx = index - 2;
+				return self.buttonAction( idx, {text: cordovaASProperties.buttonLabels[idx]} );
+			};
+
+			this.showIonicActionSheet = function () {
+				$ionicActionSheet.show( angular.extend( ionicASProperties, {
+					cancel: self.cancelAction,
+					buttonClicked: self.buttonAction,
+					destructiveButtonClicked: self.destructiveAction
+				} ) );
+			};
+
+			this.showCordovaActionSheet = function () {
+				$cordovaActionSheet.show( cordovaASProperties ).then( this.cordovaButtonActionProxy );
+			}
+		}] );
